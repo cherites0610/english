db87358747582cd442456f0a4a6fac4d2cd8ccc3
cherites <cherites0610@gmail.com>
2025-06-27 18:10:45 +0800

    fix:修正前端呼叫api網址

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(process.env.PORT ?? 3000,'0.0.0.0');
+  await app.listen(process.env.PORT ?? 8020,'0.0.0.0');
 }
 bootstrap();
 
