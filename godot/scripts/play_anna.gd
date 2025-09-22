e5cadadd047006e35ed789f9fc0ff3d7d9505ac0
feather1217 <609sophie@gmail.com>
2025-09-23 00:06:30 +0800

    feat : 對戰Anna完工

--- godot/scripts/play_anna.gd
+++ godot/scripts/play_anna.gd
@@ -2,41 +2,72 @@ extends Node2D
 
 # 透過 @onready 取得節點參考
 @onready var animated_sprite = $AnimatedSprite2D
-@onready var open_eyes_node = $張眼 # 包含張眼圖片的 Node2D
+@onready var open_eyes_node = $張眼
+@onready var body_animation_player = $AnimationPlayer
+@onready var write_animation_player = $寫字
+@onready var listen_object_node = $聽
+@onready var smile_mouth = $微笑 # 取得微笑嘴巴節點
+@onready var talking_mouth = $TalkingMouth # 新增：取得說話嘴巴節點
+
+# 定義你的變數
+var listen = false
+var is_talking = false # 新增：控制說話的變數
 
 func _ready():
-	# 初始化隨機數生成器
-	randomize() 
-	# 確保 AnimatedSprite2D 初始是隱藏的
+	randomize()
 	animated_sprite.hide()
-	# 開始眨眼計時器
+	talking_mouth.hide() # 確保說話嘴巴初始是隱藏的
 	start_blink_timer()
+	body_animation_player.play("playAnna")
+	listen_object_node.hide()
 
+func _unhandled_input(event):
+	if event is InputEventKey and event.is_pressed() and event.keycode == KEY_SPACE:
+		listen = not listen
+		if listen:
+			listen_object_node.show()
+		else:
+			listen_object_node.hide()
+	
+	# 新增：按下 'T' 鍵切換說話狀態
+	if event is InputEventKey and event.is_pressed() and event.keycode == KEY_T:
+		is_talking = not is_talking
+
+func _process(delta):
+	# 控制寫字動畫
+	if listen:
+		if not write_animation_player.is_playing():
+			write_animation_player.play("new_animation")
+	else:
+		if write_animation_player.is_playing():
+			write_animation_player.stop()
+
+	# 控制說話動畫
+	if is_talking:
+		# 當開始說話時，隱藏微笑嘴巴，顯示說話嘴巴並播放動畫
+		smile_mouth.hide()
+		talking_mouth.show()
+		if not talking_mouth.is_playing():
+			talking_mouth.play("talking")
+	else:
+		# 當停止說話時，停止動畫並隱藏說話嘴巴
+		talking_mouth.stop()
+		talking_mouth.hide()
+		# 顯示微笑嘴巴
+		smile_mouth.show()
+
+# 眨眼相關函式 (不變)
 func start_blink_timer():
-	# 選擇一個介於 2 到 6 秒之間的隨機等待時間
-	var wait_time = randf_range(2.0, 4.0) 
-	# 使用 SceneTreeTimer 進行非同步計時
+	var wait_time = randf_range(2.0, 4.0)
 	var timer = get_tree().create_timer(wait_time)
-	# 等待計時器結束
 	await timer.timeout
 	blink()
 
 func blink():
-	# 隱藏張眼的圖片
 	open_eyes_node.hide()
-	
-	# 顯示閉眼動畫節點並播放
 	animated_sprite.show()
 	animated_sprite.play("close_eyes")
-	
-	# 等待眨眼動畫播放完畢
 	await animated_sprite.animation_finished
-	
-	# 動畫播放完畢後，隱藏閉眼動畫節點
 	animated_sprite.hide()
-	
-	# 重新顯示張眼的圖片
 	open_eyes_node.show()
-	
-	# 再次開始計時器，以進行下一次眨眼
 	start_blink_timer()
